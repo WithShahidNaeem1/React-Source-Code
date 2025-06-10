4d0a988a0c61f93f770bd76374fe96f06541f43c
WithShahidNaeem1 <withshahidnaeem1@gmail.com>
2025-06-10 22:26:14 +0500

    functional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
-import CompFun from './CompFun'
+import CompFun, { flower, Provinces } from './CompFun'
+
 
 function App() {
   return (
     <>
-      <h1>Learn React</h1>
-      <h2>With Shahid Naeem</h2>
+      <h1 style={{ color: "chocolate" }}>Learn React (by) Shahid Naeem</h1>
+      {flower}
+      <Provinces />
       <CompFun />
+
     </>
 
   )
--- src/CompFun.jsx
+++ src/CompFun.jsx
@@ -2,11 +2,43 @@ function CompFun() {
     return (
         <>
             <h2>Functional Components Tutorial</h2>
-            <h2>Second Line</h2>
+            <City />
 
         </>
     )
 
 }
 
+export function Provinces() {
+    return (
+        <div>
+            <h3>Punjab</h3>
+            <h3>KPK</h3>
+            <h3>Bloachistan</h3>
+            <h3>Sindh</h3>
+
+        </div>
+
+    )
+
+}
+
+function City() {
+    return (
+        <div>
+            <h3>Karachi</h3>
+            <h3>Lahore</h3>
+            <h3>Quetta</h3>
+            <h3>Peshawar</h3>
+
+        </div>
+
+    )
+}
+
+function sum() {
+    return 5 * 5;
+}
+
+export const flower="Rose";
 export default CompFun;
