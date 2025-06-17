f4d9588111b79bb072c1d6e45f93213616661363
WithShahidNaeem1 <withshahidnaeem1@gmail.com>
2025-06-17 20:54:16 +0500

    toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-import AssignmentOne from './AssignmentOne'
+import ToggleButton from './ToggleButton'
+
 
 function App() {
     return (
     <>
       <h1 style={{ color: "chocolate" }}>Learn React (by) Shahid Naeem</h1>
-      <AssignmentOne />
+      <ToggleButton />
+     
     </>
 
   )
