1c57ecfdc3bf634fc2c0efbd2a78834125258ba2
WithShahidNaeem1 <withshahidnaeem1@gmail.com>
2025-06-22 10:54:37 +0500

    Array as Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css'
-import PropsInReact from './PropsInReact'
+import ArrayWithProps from './ArrayWithProps'
 
 function App() {
-  let name="Saad Naeem";
-  
-    return (
-    <>
-      <h1 style={{ color: "chocolate" }}>Learn React (by) Shahid Naeem</h1>
-      <PropsInReact fullName={name} age={21} adrs={'Johar Town, Lahore.'} />
-    </>
+  let university=['UET', 'UCP', 'MUL','UMT','PU'];
 
-  )
+return (
+  <>
+    <h1 style={{ color: "chocolate" }}>Learn React (by) Shahid Naeem</h1>
+    <ArrayWithProps uni={university[0]} />
+    <ArrayWithProps uni={university[1]} />
+    <ArrayWithProps uni={university[2]} />
+    <ArrayWithProps uni={university[3]} />
+    <ArrayWithProps uni={university[4]} />
+  </>
+)
 }
 
 export default App
