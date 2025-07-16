bcb548a567b9a4b5238b300f0c1a8ac32d17fbca
WithShahidNaeem1 <withshahidnaeem1@gmail.com>
2025-07-16 22:41:06 +0500

    useEffect in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import NestedLoop from "./NestedLoop"
+import UseEffect from "./UseEffect"
 
 
 function App() {
    return (
   <>
     <h1 style={{ color: "chocolate" }}>Learn React (with) Shahid Naeem</h1>
-    <NestedLoop />
+    <UseEffect />
   </>
 )
 }
--- src/Dummy.jsx
+++ src/Dummy.jsx
@@ -1,98 +1,23 @@
-import React from 'react'
-import Dummy1 from './Dummy1';
+import React, { useEffect, useState } from 'react'
 
 function Dummy() {
-    const uni=[
-        {
-            uniName: "XMT",
-            city:"Karachi",
-            email:"xmt@ymail.com",
-            courses:[
-                {
-                    course :'BSCS',
-                    duration : '4 Years',
-                    SemesterFee: 'Rs. 75000/-'
-                },
-                {
-                    course :'MBA',
-                    duration : '4 Years',
-                    SemesterFee: 'Rs. 75000/-'
-                },
-                {
-                    course :'MARKETING',
-                    duration : '4 Years',
-                    SemesterFee: 'Rs. 75000/-'
-                },
-            ]
-        },
-         {
-            uniName: "XMT",
-            city:"Karachi",
-            email:"xmt@ymail.com",
-            courses:[
-                {
-                    course :'BSCS',
-                    duration : '4 Years',
-                    SemesterFee: 'Rs. 75000/-'
-                },
-                {
-                    course :'BSCS',
-                    duration : '4 Years',
-                    SemesterFee: 'Rs. 75000/-'
-                },
-                {
-                    course :'BSCS',
-                    duration : '4 Years',
-                    SemesterFee: 'Rs. 75000/-'
-                },
-            ]
-        },
-         {
-            uniName: "XMT",
-            city:"Karachi",
-            email:"xmt@ymail.com",
-            courses:[
-                {
-                    course :'BSCS',
-                    duration : '4 Years',
-                    SemesterFee: 'Rs. 75000/-'
-                },
-                {
-                    course :'BSCS',
-                    duration : '4 Years',
-                    SemesterFee: 'Rs. 75000/-'
-                },
-                {
-                    course :'BSCS',
-                    duration : '4 Years',
-                    SemesterFee: 'Rs. 75000/-'
-                },
-            ]
-        },
-    ];
+    
+    const [count,setCount]=useState(1);
+    const [data,setData]=useState(5);
+
+    function oneTime(){
+        console.log("counter:",count,"\t" , "Data:",data);
+    }
+
+    useEffect(()=>{
+        oneTime();
+    },[count,data]);
+    
   return (
     <div>
-        <h1 style={{color:'red'}}>List of Universities</h1>
-        {
-            uni.map((uni,index)=>(
-               <div key={index} style={{
-                backgroundColor:'#ccc',
-                margin: '20px',
-                padding:'20px',
-                borderBottom:'4px solid black',
-                borderRadius:'10px',
-                width:'800px',
-                
-               }}>
-                <h1>University Name :<span style={{color:'green'}}>{uni.uniName}</span> </h1>
-                <h2>City: <span style={{color:'green'}}>{uni.city}</span></h2>
-                <h2>Email: {uni.email}</h2>
-                <h2 style={{color:'green'}}>Courses Detail</h2>
-                <Dummy1 courses={uni.courses}/>
-                </div> 
-            ))
-            
-        }
+        <button onClick={()=>setCount(count+1)}>Count {count}</button>
+        <button onClick={()=>setData(data+5)}>Data {data}</button>
+        
     </div>
   )
 }
