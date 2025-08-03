211ae4e91a6b177dba2d3bb67bd6f252b7311f89
WithShahidNaeem1 <withshahidnaeem1@gmail.com>
2025-08-03 14:59:23 +0500

    External CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import InLineStyling from "./InLineStyling"
-
+import Products from "./Products"
+import './CSS/mystyle.css'
 
 function App() {
-  
-       return (
-  <>
-    <h1 style={{ color: "chocolate" }}>Learn React (with) Shahid Naeem</h1>
-   <InLineStyling />
-  </>
-)
+  return (
+    <>
+      <h1 style={{ color: "chocolate" }}>Learn React (with) Shahid Naeem</h1>
+    <Products />
+    </>
+  )
 }
 
 export default App
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,99 +1,117 @@
 import React from 'react'
-import './myStyle.css'
+//import './CSS/mystyle.css'
 
-function Dummy1() {
+function Products() {
     return (
         <div>
-            <h1 className='heading'>External CSS</h1>
+            <h1 className='heading'>List of Products</h1>
             <div className='container'>
                 <div className='card'>
                     <div>
                         <img className='img-style' src="https://images.unsplash.com/photo-1660603135468-6e3be1a106c0?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <div>
-                        <h2 className='heading'>Wooden Table Lamp</h2>
-                        <h3>Price : Rs. 2500/-</h3>
+                        <h3 className='product'>Wooden Table Lamp</h3>
+                        <h4>Price : Rs. 2500/-</h4>
                     </div>
                 </div>
-                 <div className='card'>
+                <div className='card'>
                     <div>
                         <img className='img-style' src="https://images.unsplash.com/photo-1645477247972-c388b0c8ed6f?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <div>
-                        <h2 className='heading'>Ladies Leather Purse</h2>
-                        <h3>Price : Rs. 4500/-</h3>
+                        <h3 className='product'>Leather Shoes & Purse</h3>
+                        <h4>Price : Rs. 8500/-</h4>
                     </div>
                 </div>
-                 <div className='card'>
+                <div className='card'>
                     <div>
                         <img className='img-style' src="https://images.unsplash.com/photo-1660603135468-6e3be1a106c0?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <div>
-                        <h2 className='product'>Wooden Table Lamp</h2>
-                        <h3>Price : Rs. 2500/-</h3>
+                        <h3 className='product'>Wooden Table Lamp</h3>
+                        <h4>Price : Rs. 2500/-</h4>
                     </div>
                 </div>
-                 <div className='card'>
+                <div className='card'>
                     <div>
                         <img className='img-style' src="https://images.unsplash.com/photo-1645477247972-c388b0c8ed6f?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <div>
-                        <h2 className='heading'>Ladies Leather Purse</h2>
-                        <h3>Price : Rs. 4500/-</h3>
+                        <h3 className='product'>Leather Shoes & Purse</h3>
+                        <h4>Price : Rs. 8500/-</h4>
                     </div>
                 </div>
-                 <div className='card'>
+                <div className='card'>
                     <div>
                         <img className='img-style' src="https://images.unsplash.com/photo-1660603135468-6e3be1a106c0?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <div>
-                        <h2 className='heading'>Wooden Table Lamp</h2>
-                        <h3>Price : Rs. 2500/-</h3>
+                        <h3 className='product'>Wooden Table Lamp</h3>
+                        <h4>Price : Rs. 2500/-</h4>
                     </div>
                 </div>
-                 <div className='card'>
+                <div className='card'>
                     <div>
                         <img className='img-style' src="https://images.unsplash.com/photo-1645477247972-c388b0c8ed6f?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <div>
-                        <h2 className='heading'>Ladies Leather Purse</h2>
-                        <h3>Price : Rs. 4500/-</h3>
+                        <h3 className='product'>Leather Shoes & Purse</h3>
+                        <h4>Price : Rs. 8500/-</h4>
                     </div>
                 </div>
-                 <div className='card'>
+                <div className='card'>
                     <div>
                         <img className='img-style' src="https://images.unsplash.com/photo-1660603135468-6e3be1a106c0?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <div>
-                        <h2 className='heading'>Wooden Table Lamp</h2>
-                        <h3>Price : Rs. 2500/-</h3>
+                        <h3 className='product'>Wooden Table Lamp</h3>
+                        <h4>Price : Rs. 2500/-</h4>
                     </div>
                 </div>
-                 <div className='card'>
+                <div className='card'>
                     <div>
                         <img className='img-style' src="https://images.unsplash.com/photo-1645477247972-c388b0c8ed6f?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <div>
-                        <h2 className='heading'>Ladies Leather Purse</h2>
-                        <h3>Price : Rs. 4500/-</h3>
+                        <h3 className='product'>Leather Shoes & Purse</h3>
+                        <h4>Price : Rs. 8500/-</h4>
                     </div>
                 </div>
-                 <div className='card'>
+                <div className='card'>
                     <div>
                         <img className='img-style' src="https://images.unsplash.com/photo-1660603135468-6e3be1a106c0?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <div>
-                        <h2 className='heading'>Wooden Table Lamp</h2>
-                        <h3>Price : Rs. 2500/-</h3>
+                        <h3 className='product'>Wooden Table Lamp</h3>
+                        <h4>Price : Rs. 2500/-</h4>
                     </div>
                 </div>
-                 <div className='card'>
+                <div className='card'>
                     <div>
                         <img className='img-style' src="https://images.unsplash.com/photo-1645477247972-c388b0c8ed6f?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <div>
-                        <h2 className='heading'>Ladies Leather Purse</h2>
-                        <h3>Price : Rs. 4500/-</h3>
+                        <h3 className='product'>Leather Shoes & Purse</h3>
+                        <h4>Price : Rs. 8500/-</h4>
+                    </div>
+                </div>
+                <div className='card'>
+                    <div>
+                        <img className='img-style' src="https://images.unsplash.com/photo-1660603135468-6e3be1a106c0?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    </div>
+                    <div>
+                        <h3 className='product'>Wooden Table Lamp</h3>
+                        <h4>Price : Rs. 2500/-</h4>
+                    </div>
+                </div>
+                <div className='card'>
+                    <div>
+                        <img className='img-style' src="https://images.unsplash.com/photo-1645477247972-c388b0c8ed6f?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    </div>
+                    <div>
+                        <h3 className='product'>Leather Shoes & Purse</h3>
+                        <h4>Price : Rs. 8500/-</h4>
                     </div>
                 </div>
 
@@ -103,4 +121,4 @@ function Dummy1() {
     )
 }
 
-export default Dummy1
+export default Products
